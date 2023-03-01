927449f8e3251309ea4f32c05017e73efeb138f3
aloosli <lizzwizz@tuta.io>
2023-03-01 13:42:36 +0000

    add FAQ section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,7 +1,18 @@
 
 
 const FaqSection = () => {
-    return()
+    return(
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                </div>
+            </div>
+        </div>
+    )
 
 }
 
